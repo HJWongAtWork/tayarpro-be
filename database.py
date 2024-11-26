28e4ef3b9f54c5919e8edcd429cac29a59aa995a
Rahman Rom <rahmanrom@gmail.com>
2024-11-27 05:15:26 +0800

    latest update

--- database.py
+++ database.py
@@ -9,28 +9,28 @@ load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-# DB_HOST = os.getenv("DB_HOST")
-# DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 INSTANCE_CONNECTION_NAME = os.getenv(
-    "INSTANCE_CONNECTION_NAME", "connection_name")
+    "INSTANCE_CONNECTION_NAME", "gaia-capstone01-prd:us-central1:test-db")
 
 
-def getconn():
-    connector = Connector()
-    conn = connector.connect(INSTANCE_CONNECTION_NAME,
-                             "pg8000", user=DB_USER, password=DB_PASSWORD, db=DB_NAME)
-    return conn
+# def getconn():
+#     connector = Connector()
+#     conn = connector.connect(INSTANCE_CONNECTION_NAME,
+#                              "pg8000", user=DB_USER, password=DB_PASSWORD, db=DB_NAME)
+#     return conn
 
 
-engine = create_engine(
-    "postgresql+pg8000://", creator=getconn)
+# engine = create_engine(
+#     "postgresql+pg8000://", creator=getconn)
 
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Base = declarative_base()
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
